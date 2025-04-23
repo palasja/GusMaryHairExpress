e25416d03246ec50041cfd6b8e0993db2f8ae5d3
Yakubenka Ivan <palasja@gmail.com>
2025-04-23 18:18:09 +0300

    fix: test netlify func

--- functions/api.ts
+++ functions/api.ts
@@ -5,15 +5,15 @@ import ServerlessHttp from "serverless-http";
 
 const api = express();
 
-api.get("/.netlify/functions/api", (req, res ) => {
-  return res.json(
+api.get("/.netlify/functions/api", (_req, res ) => {
+   res.status(200).json(
     {mes: "Hello World!"}
   )
 });
 
 const handler = ServerlessHttp(api);
 
-module.export.handler = async(event, context) =>{
+module.exports.handler = async(event, context) =>{
   const result = await handler(event, context)
   return result;
 }
