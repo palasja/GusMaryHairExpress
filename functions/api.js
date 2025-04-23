316747427feefedc6d2db728b90d946a59065211
Yakubenka Ivan <palasja@gmail.com>
2025-04-23 18:11:59 +0300

    fix: test netlify func

--- functions/api.js
+++ functions/api.js
@@ -1,18 +1,17 @@
 // YOUR_BASE_DIRECTORY/netlify/functions/api.ts
 
 import express, { Router } from "express";
-import serverless from "serverless-http";
+import ServerlessHttp from "serverless-http";
 
 const api = express();
 
-const router = Router();
-api.get("/.netlify/functions/api", (req, res ) => {return res.json({mes: "Hello World!"})});
+api.get("/.netlify/functions/api", (req, res ) => {
+  return res.json(
+    {mes: "Hello World!"}
+  )
+});
 
-api.use("/api/", router);
-
-api.use("/api/hi", (req, res ) => {res.send("Hi")});
-
-const handler = serverless(api);
+const handler = ServerlessHttp(api);
 
 module.export.handler = async(event, context) =>{
   const result = await handler(event, context)
